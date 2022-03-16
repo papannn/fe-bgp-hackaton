df97a4bd5c143434d86ee5049a38c4951d44a57f
Muhammad Erlangga <erlangga.muhammad@tokopedia.com>
2022-03-16 18:02:05 +0700

    chore: init firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase"
+import { getDatabase, ref, onValue, off } from "firebase/database";
+
 import AddProduct from "./pages/addproduct";
 import Home from "./pages/home";
 import Product from "./pages/product";
@@ -9,15 +13,33 @@ import Navbar from "./components/navbar";
 import './bulma.min.css'
 import Profile from "./pages/profile";
 
+import React, { useState, useEffect } from 'react';;
+
+// init firebase
+initializeApp(firebaseConfig);
+console.log("init")
 
 const App = () => {
+  const db = getDatabase();
+  const auctionRef = ref(db, 'Auction/');
+
+  // subscribe
+  onValue(auctionRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+    // updateStarCount(postElement, data);
+  });
+
+  // detach
+  // off(auctionRef)
+
   return (
     <div className="App container">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/product/:productId" element={<Product />}/>
-        <Route path="/add_bid" element={<AddProduct />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/add_bid" element={<AddProduct />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCy6ShBzTuSfC859LmloyiiFpv6q1EeeNY",
+  authDomain: "bgp-hackathon.firebaseapp.com",
+  databaseURL: "https://bgp-hackathon-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bgp-hackathon",
+  storageBucket: "bgp-hackathon.appspot.com",
+  messagingSenderId: "994358222574",
+  appId: "1:994358222574:web:fbb83cfa216392b30c9dff",
+  measurementId: "G-92NJ82ZQQF"
+};
+
+// doc
+// https://www.bezkoder.com/react-firebase-hooks-crud/
+// https://firebase.google.com/docs/database/web/read-and-write
+
+// db realtime
+// https://console.firebase.google.com/u/1/project/bgp-hackathon/database/bgp-hackathon-default-rtdb/data
+
