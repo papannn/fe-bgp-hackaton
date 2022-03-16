ed42a752d45af9925633da4473b6fb9e3b06e7b3
Taufan Raifaldy Maaruf <taufan.maaruf@tokopedia.com>
2022-03-16 22:09:40 +0700

    Finished integrate BE

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,26 +4,12 @@ import Card from '../../components/card';
 import "./styles.css"
 
 
-const Home = () => {
-    const [productList, setProductList] = useState([
-        {
-            "product_id" : 1,
-            "user_id" : 1,
-            "name" : "Mainan anak lucu",
-            "image_url" : "https://cdn-brilio-net.akamaized.net/news/2021/07/02/209035/1509006-potret-lucu-mainan-anak.jpg",
-            "description" : "Mainan anak untuk hadiah ulang tahun, cocok buat kado natal juga bunda",
-            "start_bid" : 100000,
-            "multiple_bid" : 500,
-            "start_time" : 1647173241, //unix timestamp
-            "end_time" : 1647173241,
-            "highest_bid_id" : 1,
-            "total_bidder" : 20,
-        }
-    ])
+const Home = (state) => {
+    const productList = state.productList;
 
     const showProductList = () => {
         return productList.map((product) => {
-            return <Card {...product}/>
+            return <Card {...product} handleClickProduct={() => state.handleClickProduct(product)}/>
         })
     }
 
